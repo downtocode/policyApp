e63744e2bae925c9037450139336c23f68844d51
Philip <pkinasha@gmail.com>
2016-02-01 16:19:21 -0500

    mongodb update

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-var uri = "mongodb://heroku_app35837312:jfl6ps1binv31ke665qm7ss99f@ds061731.mongolab.com:61731/heroku_app35837312?replicaSet=rs-ds061731",
+var uri = "mongodb://heroku_bg8bldkt:tbp5982u42triu0ow2bkb47227@ds055525.mongolab.com:55525/heroku_bg8bldkt?replicaSet=rs-ds055525",
     db = mongojs(uri, ["test", "questions", "users", "accounts", "userAnswers", "friends", "demographics", "petitions", "coefficients", "userLikes"]);
 
 app.use(function(req,res,next) { 
